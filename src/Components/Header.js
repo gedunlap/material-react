a1400bbb82549b68f873cccd44b02f044d828800
gedunlap <g.e.dunlap90@gmail.com>
2021-08-03 12:27:07 -0400

    starts nav setup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom'
+
 function Header(props){
-    return <h1>Header</h1>
+    return (
+        <nav className="nav">
+            <Link to="/" className="navimg">
+                <img src="https://i.imgur.com/S10uG78.jpg" alt="" />
+            </Link>
+            <Link to="/" className="navitem">
+                <div>Home</div>
+            </Link>
+            <Link to="/about" className="navitem">
+                <div>About</div>
+            </Link>
+        </nav>
+    )
   } 
   
   export default Header
