c0d6d6aa0a217c960cf8f3ae544b2139f063bccb
gedunlap <g.e.dunlap90@gmail.com>
2021-08-23 13:23:16 -0400

    location page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,9 @@
 function About(props){
-    return <h1>About</h1>
+    return (
+      <div className="about">
+        <h1>About</h1>
+      </div>
+    )
   } 
   
   export default About
--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -1,9 +1,15 @@
 function Location(props){
-    return (
-      <div className="locationpg">
-        <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d669.2123632943091!2d-82.46043220633995!3d27.95809178048827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2c46409771069%3A0x5b05cd55cda477e4!2s1601%20N%20Franklin%20St%2C%20Tampa%2C%20FL%2033602!5e0!3m2!1sen!2sus!4v1628525438665!5m2!1sen!2sus" width="80%" height="40%" style={{border:0}} allowfullscreen="" loading="lazy"></iframe>
+  return (
+    <div className="locationpg">
+      <div className="locationinfo">
+        <img src="https://i.imgur.com/GBqxl8D.png" alt="store front" />
+        <h3>Located on the border of Tampa Heights and the Downtown corridor, just minutes from Armature Works.</h3>
       </div>
-    )
-  } 
+      <div className="locationmap">
+        <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d669.2123632943091!2d-82.46043220633995!3d27.95809178048827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2c46409771069%3A0x5b05cd55cda477e4!2s1601%20N%20Franklin%20St%2C%20Tampa%2C%20FL%2033602!5e0!3m2!1sen!2sus!4v1628525438665!5m2!1sen!2sus" width="" height="" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
+      </div>
+    </div>
+  )
+} 
   
   export default Location
